14115ea5b48ae96edeb17243b3bd6c13949d0a6a
squirrelabbit <wjddmsdnjs1@naver.com>
2022-02-16 17:46:50 +0900

    22/02/16 algorithm classday

---
+++ algorithm/cardgame1.py
@@ -0,0 +1,11 @@
+cards = [4, 8, 7, 3, 1, 5, 5, 6]
+cards_count=[0]*10
+for card in cards :
+    cards_count[card] += 1
+
+print(cards_count)
+
+
+
+
+
---
+++ algorithm/cardgame2.py
@@ -0,0 +1,6 @@
+cards = [4, 8, 7, 3, 1, 5, 5, 6]
+cards_count=[0]*10
+for card in cards :
+    cards_count[card]+=1
+
+cards_count
